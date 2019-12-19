3cee1a2d456de4d23a231de6cdb512df6f584a39
balde-soul <CJHdream@outlook.com>
2019-12-19 22:01:15 +0800

    complete multi_language api

--- multi_language/multi_language.cpp
+++ multi_language/multi_language.cpp
@@ -1,7 +1,22 @@
 #include <ProjectBase/multi_language/multi_language.hpp>
+//Languages
+Languages::Languages(){}
+Languages::Languages(const Languages& other){}
+Languages::Languages(Languages&& right){}
 
-MultiLanguage::MultiLanguage(){};
-MultiLanguage::~MultiLanguage(){};
-void MultiLanguage::test(){};
-void MultiLanguage::tttttt(){};
-void MultiLanguage::ttttt(){};
+//Language
+Language::Language(){}
+Language::Language(const Language& other){}
+Language::Language(Language&& right){}
+
+//LanguageIndex
+LanguageIndex::LanguageIndex(){}
+LanguageIndex::LanguageIndex(const LanguageIndex& other){}
+LanguageIndex::LanguageIndex(LanguageIndex&& right){}
+
+// MultiLanguage
+MultiLanguage::MultiLanguage(){}
+MultiLanguage::~MultiLanguage(){}
+void MultiLanguage::SetLanguageFold(const std::string& pth){}
+void MultiLanguage::SetLanguage(const Language& name){}
+const std::string& MultiLanguage::Extract(const LanguageIndex& index){return index;}
--- multi_language/multi_language.hpp
+++ multi_language/multi_language.hpp
@@ -1,22 +1,51 @@
 #ifndef PROJECT_BASE_MULTI_LANGUAGE_H
 #define PROJECT_BASE_MULTI_LANGUAGE_H
+#include <string>
 #include <ProjectBase/multi_language/Define.hpp>
+class PROJECT_BASE_MULTI_LANGUAGE_SYMBOL Languages{
+public:
+    Languages();
+    Languages(const Languages& other);
+    Languages(Languages&& right);
+public:
+};
+
+class PROJECT_BASE_MULTI_LANGUAGE_SYMBOL Language{
+public:
+    Language();
+    Language(const Language&);
+    Language(Language&& right);
+};
+
+class PROJECT_BASE_MULTI_LANGUAGE_SYMBOL LanguageIndex{
+public:
+    LanguageIndex();
+    LanguageIndex(const std::string& index);
+    LanguageIndex(const LanguageIndex& other);
+    LanguageIndex(LanguageIndex&& right);
+public:
+    operator std::string() const;
+};
+
+class multi_language;
 class PROJECT_BASE_MULTI_LANGUAGE_SYMBOL MultiLanguage
 {
 public:
     MultiLanguage();
-    //MultiLanguage(MultiLanguage &&) = delete;
-    //MultiLanguage(const MultiLanguage &) = delete;
-    //MultiLanguage &operator=(MultiLanguage &&) = delete;
-    //MultiLanguage &operator=(const MultiLanguage &) = delete;
+    MultiLanguage(MultiLanguage &&) = delete;
+    MultiLanguage(const MultiLanguage &) = delete;
+    MultiLanguage &operator=(MultiLanguage &&) = delete;
+    MultiLanguage &operator=(const MultiLanguage &) = delete;
     ~MultiLanguage();
 public:
-    void test();
-    void ttttt();
-    void tttttt();
-
-private:
+    void SetLanguage(const Language& language);
+    void SetLanguageFold(const std::string& pth);
+public:
+    const Languages& SupportedLanguages() const;
+    const std::string& Extract(const LanguageIndex& index);
     
+public:
+    multi_language* _impl;
 };
 #else
 #endif // !1_H_MULTI_LANGUAG
