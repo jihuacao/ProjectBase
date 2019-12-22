9996a236fd408cb27e5b4d30d4593dae5979b138
balde-soul <CJHdream@outlook.com>
2019-12-22 22:21:44 +0800

    complete external_setting, popular_message.system_message

--- external_process/external_setting.cmake
+++ external_process/external_setting.cmake
@@ -1,3 +1,3 @@
 option(external_install_path "the path for installing the external" /usr/local/)
-option(external_build_type "the type(release or debug) for building the external" Release)
-option(external_build_platform "the platform for building the external" x64)
+option(external_build_type "the type(release or debug) for building the external" ${CMAKE_BUILD_TYPE})
+option(external_build_shared "the lib link type for building the external" ON)
--- external_process/googleflags.cmake
+++ external_process/googleflags.cmake
@@ -1,7 +1,17 @@
 message(${CMAKE_CURRENT_LIST_FILE})
+include(popular_message)
 include(ExternalProject)
+project_base_system_message()
 if(CMAKE_SYSTEM_NAME MATCHES "Windows")
-    message("WIN")
+    if(CMAKE_BUILD_TYPE MATCHES "Debug")
+        #gflags_debug.lib
+        #gflags_no_threads_debug.lib
+    elseif(CMAKE_BUILD_TYPE MATCHES "Release")
+    endif()
 elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
-    message("linux")
+    if(CMAKE_BUILD_TYPE MATCHES "Debug")
+    elseif(CMAKE_BUILD_TYPE MATCHES "Release")
+        #libgflags.so
+        #libgflags_nothreas.so
+    endif()
 endif()
