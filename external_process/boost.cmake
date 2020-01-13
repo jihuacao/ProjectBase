44c9bf05ae82f788f8d27445fa5a5209789b809b
balde-soul <CJHdream@outlook.com>
2020-01-13 17:05:19 +0800

    boost waiting for completion

--- external_process/boost.cmake
+++ external_process/boost.cmake
@@ -4,8 +4,12 @@ cmakelists_base_header()
 project_base_system_message()
 include(ExternalProject)
 include(file_read_method)
+include(file_operation)
 set(module Boost)
 set(external_project_add_target _boost)
+set(url_label_name boost)
+set(extension tar.gz)
+set(component_lib_prefix Boost)
 
 # generate a boost component config file to the CMAKE_SOURCE_DIR
 ## find the config file
@@ -43,7 +47,7 @@ list(
 
 set(
     ${module}_supported_url 
-    https://dl.bintray.com/${module}org/release/1.71.0/source/${module}_1_71_0.tar.gz
+    https://dl.bintray.com/${url_label_name}org/release/1.71.0/source/${url_label_name}_1_71_0.tar.gz
     )
 
 set(
@@ -87,51 +91,68 @@ message(STATUS "Prefix Configuration:
 ````````${module}_VERBOSE: ${${module}_VERBOSE}
 ````````${module}_DEBUG: ${${module}_DEBUG}")
 
-find_package(${module} "1.71" COMPONENTS ${${module}_with} CONFIG HINTS /home/sins/Download/boost_1_71_0/install/)
+find_package(${module} "1.71" COMPONENTS ${${module}_with} CONFIG HINTS ${external_install_path})
 
 function(download_and_build_boost)
     version_url_hash_match(${module} ${module}_all_version ${module}_supported_url ${module}_supported_hash ${module}_version)
+    
+    string(REPLACE "." "_" url_version_name ${${module}_version})
+    string(CONCAT download_file_name ${url_label_name}_ ${url_version_name}.${extension})
 
-    string(REPLACE "." "_" boost_download_file_name boost.${${module}_version})
-    find_file(found_download_file ${boost_download_file_name} ${external_download_path})
-    set(actual_download_command axel -k -n 10 -av ${${module}_url} -o ${external_download_dir}/${boost_download_file_name})
-    if(${found_download_file} STREQUAL "found_download_file-NOTFOUND")
-        set(download_command ${actual_download_command})
+    touch_file(target module ${module}_url ${module}_hash external_download_dir download_file_name)
+
+    # get the component option
+    set(with_component_option)
+    foreach(component ${${module}_with})
+       set(with_component_option ${component},${with_component_option})
+    endforeach()
+    message(STATUS "with_component_option: ${with_component_option}")
+    if(${_${module}_build_type} STREQUAL "RELEASE")
+        set(_variant release)
     else()
-        file(SHA256 ${external_download_dir}/${boost_download_file_name} exist_sha)
-        if(${exist_sha} STREQUAL ${${module}_hash})
-            set(download_command )
-        else()
-            set(download_command ${actual_download_command})
-        endif()
+        set(_variant debug)
     endif()
-    unset(actual_download_command)
-    add_custom_command(OUTPUT a COMMAND echo asd COMMAND echo aswf)
-    message(FATAL_ERROR "ad")
-
-    # mkdir the 
-    add_custom_command()
-    ExternalProject_Add(
-        _boost
-        PREFIX ${module}-${${module}_version}
-        URL ${${module}_url}
-        URL_HASH:SHA256="${${module}_hash}"
-        DOWNLOAD_COMMAND ${download_command}
-        DOWNLOAD_DIR "${external_download_dir}"
-        UPDATE_COMMAND ""
+    if(${_${module}_build_shared})
+        set(_link shared)
+    else()
+        set(_link static)
+    endif()
+    
+    add_custom_target(_${module}
+        COMMAND ${CMAKE_COMMAND} -E chdir ${external_download_dir} tar -xvf ${download_file_name} >./decompression_info.txt
+        COMMAND ${CMAKE_COMMAND} -E chdir ${external_download_dir}/${url_label_name}_${url_version_name} ./bootstrap.sh --with-libraries=${with_component_option} --prefix=${external_install_path} variant=_variant link=_link runtime-link=shared threading=multi
+        COMMAND ${CMAKE_COMMAND} -E chdir ${external_download_dir}/${url_label_name}_${url_version_name} ./b2
+        COMMAND ${CMAKE_COMMAND} -E chdir ${external_download_dir}/${url_label_name}_${url_version_name} ./b2 install --prefix=${external_install_path}
     )
-    unset(boost_download_file_name)
-    unset(found_download_file)
-    unset(download_command)
+    add_dependencies(_${module} ${target})
+    unset(with_component_option)
+    unset(_variant)
+    unset(_link)
 endfunction(download_and_build_boost)
+
+function(build_boost_target version components install_path)
+    foreach(component ${components})
+        add_library(${component_lib_prefix}::${component} UNKNOW IMPORTED)
+        set_target_properties(
+            ${component_lib_prefix}::${component}
+            PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${${install_path}}/include"
+            INTERFACE_COMPILE_DEFINITIONS "BOOST_ALL_NO_LIB"
+        )
+    endforeach()
+endfunction(build_boost_target)
 
 macro(add_total_boost_component_link)
     add_library(${module}_total UNKNOWN IMPORTED)
     foreach(component ${${module}_with})
-        set_property(TARGET ${module}_total APPEND PROPERTY INTERFACE_LINK_INTERFACE_LIBRARIES_RELEASE "${module}::${component}")
+        #set_property(TARGET ${module}_total APPEND PROPERTY IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "${module}::${component}")
+        add_dependencies(${module}_total ${module}::${component})
     endforeach()
-    get_target_property(t ${module}_total INTERFACE_LINK_INTERFACE_LIBRARIES_RELEASE)
-    message(STATUS "${t}")
+    show_target_properties(Boost::filesystem)
+    set_target_properties(${module}_total
+        PROPERTIES 
+        INTERFACE_LINK_LIBRARIES "Boost::filesystem"
+    )
     set(${module}_target_name ${module}_total)
     message(STATUS ${${module}_target_name})
 endmacro(add_total_boost_component_link)
