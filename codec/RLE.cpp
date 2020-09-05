e68673ce7bb0cbcfd89f43f90070ecca5967f2f6
balde-soul <CJHdream@outlook.com>
2020-09-05 19:19:32 +0800

    update

--- codec/RLE.cpp
+++ codec/RLE.cpp
@@ -1,56 +1,9 @@
-
-// ================================================================================================
-// -*- C++ -*-
-// File: rle.hpp
-// Author: Guilherme R. Lampert
-// Created on: 16/02/16
-// Brief: Simple Run Length Encoding (RLE) in C++11.
-// ================================================================================================
-
-#ifndef RLE_HPP
-#define RLE_HPP
-
-// ---------
-//  LICENSE
-// ---------
-// This software is in the public domain. Where that dedication is not recognized,
-// you are granted a perpetual, irrevocable license to copy, distribute, and modify
-// this file as you see fit.
-//
-// The source code is provided "as is", without warranty of any kind, express or implied.
-// No attribution is required, but a mention about the author is appreciated.
-//
-// -------
-//  SETUP
-// -------
-// #define RLE_IMPLEMENTATION in one source file before including
-// this file, then use rle.hpp as a normal header file elsewhere.
-//
-// RLE_WORD_SIZE_16 #define controls the size of the RLE word/count.
-// If not defined, use 8-bits count.
-
-#include <cstdint>
-
-namespace rle
-{
-
-// RLE encode/decode raw bytes:
-int easyEncode(const std::uint8_t * input, int inSizeBytes, std::uint8_t * output, int outSizeBytes);
-int easyDecode(const std::uint8_t * input, int inSizeBytes, std::uint8_t * output, int outSizeBytes);
-
-} // namespace rle {}
-
-// ================== End of header file ==================
-#endif // RLE_HPP
-// ================== End of header file ==================
-
 // ================================================================================================
 //
 //                                     RLE Implementation
 //
 // ================================================================================================
-
-#ifdef RLE_IMPLEMENTATION
+#include "codec/RLE.hpp"
 
 namespace rle
 {
@@ -174,7 +127,3 @@ int easyDecode(const std::uint8_t * input, const int inSizeBytes, std::uint8_t *
 }
 
 } // namespace rle {}
-
-// ================ End of implementation =================
-#endif // RLE_IMPLEMENTATION
-// ================ End of implementation =================
