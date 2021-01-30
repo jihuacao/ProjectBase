2849d0d6fa60bf33eed628e5c5f688ab5ddf5da9
balde-soul <CJHdream@outlook.com>
2021-01-30 21:49:05 +0800

    remain the boost debug name,可以得知的是，boost无论指定debug还是release生成的文件名一致，可以通过readlef进行区分

--- util/util_test/test_quick_sort.cc
+++ util/util_test/test_quick_sort.cc
@@ -5,7 +5,7 @@
 
 TEST(Sort, QuickSort){
     int len = 10;
-    int a[len] = {0, 1, 2, 2, 3, 4, 5, 6, 7, 8};
+    int a[len] = {0, 10, 2, 2, 3, 4, 5, 6, 7, 8};
     quick_sort(a, 0, len);
     for(auto i = 0; i < len; ++i) std::cout << a[i];
     std::cout << std::endl;
--- util/util_test/util_test.cc
+++ util/util_test/util_test.cc
@@ -1,8 +1,8 @@
 #include <ProjectBase/util/sort.hpp>
 #include <gtest/gtest.h>
 
-int main(int* argc, char** argv){
-    testing::InitGoogleTest(argc, argv);
+int main(int argc, char** argv){
+    testing::InitGoogleTest(&argc, argv);
     auto a = RUN_ALL_TESTS();
     return a;
 }
