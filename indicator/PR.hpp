89a4888351d4a1a68db7a80b2521bb53c84d3a38
balde-soul <CJHdream@outlook.com>
2021-02-26 21:45:36 +0800

    PR-1

--- indicator/PR.hpp
+++ indicator/PR.hpp
@@ -1,4 +1,6 @@
-#include <vector>
+#include <boost/container/vector.hpp>
+#include <boost/timer.hpp>
+#include <boost/assert.hpp>
 
 struct pr{
     float percision;
@@ -12,14 +14,39 @@ struct pn{
     unsigned long long FN;
 };
 
-unsigned long long get_tp(std::vector<double>& score, std::vector<bool>& label){
+unsigned long long get_tp(boost::container::vector<double>& score, boost::container::vector<bool>& label);
+
+/**\brief
+ * \note
+ * \param[in] label
+ * \param[out] p
+ * \param[out] n
+ * */
+template<typename In, typename Out>
+unsigned long long get_label_p_n(boost::container::vector<In>& label, Out* const & p, Out* const & n){
+    for(auto iter = label.begin(); iter != label.end(); ++iter){
+        if(*iter == 1){
+            (*p)++;
+        }
+        else{
+            (*n)++;
+        }
+    }
+    return NULL;
 }
 
-std::vector<pr> PR(std::vector<double>& score, std::vector<bool>& label){
-    int TP = ;
+template<typename T> T p(const T& TP){
+
+}
+
+boost::container::vector<pr> PR(boost::container::vector<double>& score, boost::container::vector<double>& label){
+    int TP = 0;
     int TN = 0;
-    int FP = ;
+    int FP = 0;
     int FN = 0;
+    unsigned long long p = 0;
+    unsigned long long n = 0;
+    BOOST_ASSERT(get_label_p_n(score, &p, &n) == NULL);
     for(auto index = 0; index < score.size(); ++index){
         if(label[index] == 0){
         }
--- indicator/indicator_test/indicator_test.cc
+++ indicator/indicator_test/indicator_test.cc
@@ -1,3 +1,9 @@
+#include <boost/container/vector.hpp>
+#include "../PR.hpp"
+
 int main(int argc, char** argv){
+    auto pre = boost::container::vector<double>({0, 1, 1, 0, 0, 0, 1, 0, 1 , 1, 1, 1});
+    auto label = boost::container::vector<double>({0, 1, 1, 0, 0, 0, 1, 0, 1 , 1, 1, 1});
+    PR(pre, label);
     return 0;
 }
