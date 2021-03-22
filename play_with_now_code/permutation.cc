763219a91ed40c32f5481b4223afc1d2b707a36a
balde-soul <CJHdream@outlook.com>
2021-03-22 13:52:38 +0800

    增加Permutation.Unrepeated的说明

--- play_with_now_code/permutation.cc
+++ play_with_now_code/permutation.cc
@@ -6,6 +6,12 @@
 #include <unordered_map>
 #include <queue>
 
+/**
+ * \brief brief TotalPermutation.Unrepeated
+ * \note note 题目要求：给定一个vector，里面不包含重复元素，程序给出所有可能排序的结果
+ * \author none
+ * \since version
+ * */
 typedef int TotalPermutationUnrepeatedInputType;
 typedef std::vector<TotalPermutationUnrepeatedInputType> TotalPermutationUnrepeatedInput;
 typedef std::unordered_map<TotalPermutationUnrepeatedInputType, int> TotalPermutationStatus;
