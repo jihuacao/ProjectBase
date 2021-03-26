72e8a46e44ce846bb1fe915aef628f72b1d7022c
balde-soul <CJHdream@outlook.com>
2021-03-26 15:28:10 +0800

    build method framework

---
+++ method/char_operation.hpp
@@ -0,0 +1,46 @@
+/****************************************************************************
+<one line to give the program's name and a brief idea of what it does.>
+Copyright (C) <year>  <name of author>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*****************************************************************************/
+/**
+ * @file 文件名
+ * @brief 简介
+ * @details 细节
+ * @mainpage 工程概览
+ * @author 作者
+ * @email 邮箱
+ * @version 版本号
+ * @date 年-月-日
+ * @license 版权
+ */
+//#include <ProjectBase/method/Define.hpp>
+///**
+// * \brief CharOperation
+// * \note note 实现基于字符串的四则运算
+// * \author JiHuaCao
+// * \since 2021/3/26
+// * */
+//namespace ProjectBase{
+//    namespace Method{
+//        class CharOperation {
+//            public:
+//                CharOperation(){};
+//            public:
+//                static template<typename _T> unsigned long long Add(char* a_ptr, size_t a_size, char* b_ptr, size_t b_size){
+//                    size_of(_T);
+//                    return 0;
+//                };
+//        };
+//    };
+//};
