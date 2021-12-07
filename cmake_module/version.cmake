5d35bd124b888492528406310fa464b03b16fd47
jihuacao <CJHdream@Outlook.com>
2021-12-07 17:26:41 +0800

    add the version from git

---
+++ cmake_module/version.cmake
@@ -0,0 +1,18 @@
+﻿#[[
+    function_name: 
+]]
+function(version_from_git major minor maintenance build)
+    include(cmake/extract_information_from_git.cmake)
+    git_current_tag(git_tag)
+    git_current_commit(git_commit)
+    string(REPLACE "." ";" a ${git_tag})
+    list(LENGTH a len)
+    message(STATUS "len:${len}")
+    list(GET a 0 get)
+    set(${major} ${get} PARENT_SCOPE)
+    list(GET a 1 get)
+    set(${minor} ${get} PARENT_SCOPE)
+    list(GET a 2 get)
+    set(${maintenance} ${get} PARENT_SCOPE)
+    set(${build} ${git_commit} PARENT_SCOPE)
+endfunction(version_from_git)
