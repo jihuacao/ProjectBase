d3da6f3a16d8a18c8ea98629c4132b1e56521b3a
”jihuacao“ <CJHdream@outlook.com>
2022-03-22 21:34:58 +0800

    update

--- build/build.sh
+++ build/build.sh
@@ -3,18 +3,15 @@
 }
 ARGS=`getopt \
     -o hab:c:: \
-    --long help,along,blong:,clong:: \
+    --long help,along,blong:,component:: \
     -n 'example.bash' -- "$@"`
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "${ARGS}"
-while true ; do # 对与参数字符的遍历不结束，该项永远为true
+while true ; do
     case "$1" in
-        ## 由于a是属于无参数值类型，因此只需要shift 1
         -a|--along) echo "Option $1"; shift 1;;
-        ## 由于b，b-long是有参数值类型，则需要shift 2，使用$2来获得值
         -b|--blong) echo "Option $1, argument $2"; shift 2;;
-        -c|--clong)
-            ## 由于c，c-long是一个option类型的参数，因此需要使用case进行强约束，避免混乱
+        -c|--component)
             case "$2" in
                 "") echo "Option $1, no argument"; shift 2;;
                 *)  echo "Option $1, argument $2" ; shift 2;;
