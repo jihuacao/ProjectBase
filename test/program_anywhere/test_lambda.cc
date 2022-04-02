2da520710b5273eea8de7de7611e5d3c7bb3bfcc
”jihuacao“ <CJHdream@outlook.com>
2022-04-02 22:22:35 +0800

    add the lambda test

---
+++ test/program_anywhere/test_lambda.cc
@@ -0,0 +1,16 @@
+﻿#include <functional>
+#include <gtest/gtest.h>
+
+std::function<int(int&)> generate_lambda_function(int& t1){
+    return [&](int& t2) -> int{
+        std::swap(t1, t2);
+        return 0;
+    };
+};
+
+TEST(B, B){
+    int t1 = 1;
+    int t2 = 2;
+    auto func = generate_lambda_function(t1);
+    ASSERT_EQ(func(t2), 0);
+}
