616ace290edd29b50712537a2e3fb29e2da0cdf8
jihuacao <CJHdream@outlook.com>
2023-07-26 19:29:25 +0800

    update

--- tools/x64_makefile_gcc.sh
+++ tools/x64_makefile_gcc.sh
@@ -5,6 +5,9 @@ workroot=$(dirname $(cd $(dirname $0); pwd))
 Platform=x64
 Configuration=DEBUG
 BuildDir=${call_dir}/BinaryOut/${Platform}-${Configuration}
+mkdir -p ${BuildDir}
+InstallDir=${call_dir}/InstallOut/${Platform}-${Configuration}
+mkdir -p ${InstallDir}
 
 source ${bash_dir}/build.sh \
 --build_dir=${BuildDir} \
@@ -13,4 +16,5 @@ source ${bash_dir}/build.sh \
 --generator="Unix Makefiles" \
 --compiler=gcc \
 --build_share \
---linker=ld
+--linker=ld \
+--build-options=" -DCMAKE_INSTALL_PREFIX=${InstallDir}"
